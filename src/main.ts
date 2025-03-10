b60e7d1e30d2cc3618e3d453570d61d323643c46
Danilo Abril_09 <dacojose0@gmail.com>
2025-03-09 20:54:11 -0700

    added validations for users module

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
       transformOptions: {
         enableImplicitConversion: true,
       },
