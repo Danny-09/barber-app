a394714ddd3f9f055349732ba6aa1c670482d876
Danilo Abril_09 <dacojose0@gmail.com>
2025-03-27 17:04:44 -0700

    Add default roles to roles table during migration

--- src/database/migrations/1741199006725-CreateRolesTable.ts
+++ src/database/migrations/1741199006725-CreateRolesTable.ts
@@ -5,6 +5,13 @@ export class CreateRolesTable1741199006725 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`roles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        // Insertar roles por defecto
+        await queryRunner.query(`
+            INSERT INTO \`roles\` (\`name\`) VALUES 
+            ('SUPER_ADMIN'), 
+            ('BARBER'), 
+            ('CUSTOMER');
+        `);
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`phone\` varchar(255) NOT NULL, \`role_id\` int NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_a2cecd1a3531c0b041e29ba46e1\` FOREIGN KEY (\`role_id\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
